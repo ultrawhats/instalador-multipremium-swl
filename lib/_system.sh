bc341f5957495f3b6bd4108c5c4608215fb0a8ea
UltraWhats <117473567+ultrawhats@users.noreply.github.com>
2024-07-03 15:18:31 -0300

    Update _systema

--- lib/_system.sh
+++ lib/_system.sh
@@ -14,13 +14,13 @@ system_create_user() {
 
   sleep 2
 
-  #sudo su - root <<EOF
-  #useradd -m -p $(openssl passwd -crypt ${mysql_root_password}) -s /bin/bash -G sudo deploy
-  #usermod -aG sudo deploy
-
   sudo su - root <<EOF
-  useradd -m -p $(openssl passwd ${mysql_root_password}) -s /bin/bash -G sudo deploy
+  useradd -m -p $(openssl passwd -crypt ${mysql_root_password}) -s /bin/bash -G sudo deploy
   usermod -aG sudo deploy
+
+  #sudo su - root <<EOF
+  #useradd -m -p $(openssl passwd ${mysql_root_password}) -s /bin/bash -G sudo deploy
+  #usermod -aG sudo deploy
 
 EOF
 
